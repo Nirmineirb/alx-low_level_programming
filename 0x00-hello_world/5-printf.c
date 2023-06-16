6ad62dfadc2ff9db489979b7de3b79afb521960d
NirmineIrb <nirminirb@gmail.com>
2023-06-15 17:50:03 -0700

    ok

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 /**
- * main - A program that print a line using the print function 
+ * main - A program that print a line using the printf function 
  *
  * Return: 0 (Success)
  */
