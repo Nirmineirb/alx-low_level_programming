fd535c585ea7608d5b9cea3f61d22147e6e78583
NirmineIrb <nirminirb@gmail.com>
2023-06-17 08:23:13 -0700

    ok

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+	char number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+	}
+	for (number = 'a'; number <= 'f'; number++)
+	{
+		putchar(number);
+	}
+
+	putchar('\n');
+	return (0);
+}
